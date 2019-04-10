3a531b63760a3275fcdb242c30dcb76d0185e5c5
Fernando Martinez <fmartinez@NSL-FMARTINEZs-MacBook-Pro.local>
2019-04-10 08:26:41 -0600

    caching: Add caching of pokemon.

--- src/main/java/fmartin1/pokedex/repository/PokemonRepository.java
+++ src/main/java/fmartin1/pokedex/repository/PokemonRepository.java
@@ -2,17 +2,22 @@ package fmartin1.pokedex.repository;
 
 import fmartin1.pokedex.model.pokemon.Pokemon;
 import fmartin1.pokedex.model.pokemon.generation.Generation;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
 
+    @Cacheable("pokemon")
     Pokemon findByName(String name);
 
+    @Cacheable("pokemon")
     Pokemon findById(int id);
 
+    @Cacheable("pokemon")
     List<Pokemon> findByType1OrType2(String name, String name1);
 
+    @Cacheable("pokemon")
     List<Pokemon> findByGeneration(Generation generation);
 }
